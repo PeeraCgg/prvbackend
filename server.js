e5caa5a93442f38dd69d14e07fc953e6e56269fc
PeeraCgg <peerawat.umtoeworks@gmail.com>
2024-11-28 08:27:42 +0700

    backendprv2

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import AuthRoutes from './routes/auth.routes.js';
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -17,6 +18,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // cors
 app.use(cors());
 
+// Serve static files
+const __dirname = path.resolve(); // ใช้ path.resolve() เพื่อกำหนด __dirname
+app.use(express.static(path.join(__dirname, 'dist'))); // ให้ Express เสิร์ฟไฟล์จากโฟลเดอร์ dist
+
+
+// Fallback route for SPA
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html')); // ส่งกลับ index.html สำหรับทุก request ที่ไม่ตรงกับ API
+});
 //api way
 app.use('/user', AuthRoutes)
 // listen on port
